303cbdccc1e06f54576be8b8f547ee99b8564da7
Rafael Cabrera <33456559+rafaelcabrera@users.noreply.github.com>
2022-10-06 20:43:11 -0300

    setup inicial listo

---
+++ src/components/PokemonList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import PokemonCard from "./PokemonCard";
+import './PokemonList.css';
+
+const PokemonList = ({ pokemons }) =>{
+    return <div className="PokemonList">
+        {pokemons.map((pokemon) => {
+        return <PokemonCard />;
+})}
+    </div>;
+}
+
+PokemonList.defaultProps = {
+    pokemons: Array(10).fill('autofill')
+}
+
+export default PokemonList;
