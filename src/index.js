1cf3f938148e904442f0501178a3e0db856c5617
Rafael Cabrera <33456559+rafaelcabrera@users.noreply.github.com>
2022-10-08 03:42:51 -0300

    custom middleware

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import App from './App';
-import { pokemonsReducer } from './reducers/pokemons';
-import { legacy_createStore as createStore} from 'redux';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import { pokemonsReducer } from "./reducers/pokemons";
+import {
+  applyMiddleware,
+  compose,
+  legacy_createStore as createStore,
+} from "redux";
+import { logger } from "./middlewares";
+import "./index.css";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const store= createStore(pokemonsReducer);
+const composedEnhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger)
+);
+
+const store = createStore(pokemonsReducer, composedEnhancers);
+
 root.render(
   <React.StrictMode>
-    <Provider store={store} >
-    <App />
+    <Provider store={store}>
+      <App />
     </Provider>
   </React.StrictMode>
 );
