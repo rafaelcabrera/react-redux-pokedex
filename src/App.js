ca3159cd05345d1d0698bc3bd61332c50a118267
Rafael Cabrera <33456559+rafaelcabrera@users.noreply.github.com>
2022-10-08 03:26:11 -0300

    redux with hooks

--- src/App.js
+++ src/App.js
@@ -1,45 +1,46 @@
-import React from 'react';
-import {useEffect} from 'react'
-import { connect } from 'react-redux'
-import logo from './statics/logo.svg'
-import Searcher from './components/Searcher';
-import { Col } from 'antd';
-import './App.css';
-import {getPokemon } from './api'
-import PokemonList from './components/PokemonList';
-import {setPokemons as setPokemonsActions} from './actions'
+import React from "react";
+import { useEffect } from "react";
+// import { connect } from 'react-redux'
+import { useDispatch, useSelector } from "react-redux";
+import logo from "./statics/logo.svg";
+import Searcher from "./components/Searcher";
+import { Col } from "antd";
+import "./App.css";
+import { getPokemon } from "./api";
+import PokemonList from "./components/PokemonList";
+import { setPokemons } from "./actions";
 
-function App({pokemons, setPokemons}) {
+function App() {
   // const [pokemons,setPokemons] = useState([]);
-  useEffect(() =>{
+  const pokemons = useSelector((state => state.pokemons))
+  const dispatch= useDispatch();
+  useEffect(() => {
     const fetchPokemons = async () => {
-      const pokemonsRes= await getPokemon();
-      setPokemons(pokemonsRes)
+      const pokemonsRes = await getPokemon();
+      dispatch(setPokemons(pokemonsRes)); //agrego el dispatcher de aca
     };
     fetchPokemons();
   }, []);
   return (
-    <div className='App'>
+    <div className="App">
       <Col span={4} offset={10}>
-      <img src={logo} alt='Pokedex'/>
+        <img src={logo} alt="Pokedex" />
       </Col>
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
-      <PokemonList pokemons ={pokemons}/>
+      <PokemonList pokemons={pokemons} />
     </div>
   );
 }
 
-// export default App;
+export default App;
 
-const mapStateToProps=(state)=>({pokemons:state.pokemons,}); //recibir estado y retorna un objeto cuyas propiedades va a enviar al compoennte que se conecta a redux
-// const mapStateToProps=(state)=>({pokemons:state.pokemons,});
+// const mapStateToProps=(state)=>({pokemons:state.pokemons,}); //recibir estado y retorna un objeto cuyas propiedades va a enviar al compoennte que se conecta a redux
+// // const mapStateToProps=(state)=>({pokemons:state.pokemons,});
 
+// const mapDispatchToProps = (dispatch)=> ({setPokemons: (value) => dispatch(setPokemonsActions(value)),}); //recibe el dispatcher de redux y retorna el objeto mapeado pero ahora con los actions creators que habíamos establecido.
 
-const mapDispatchToProps = (dispatch)=> ({setPokemons: (value) => dispatch(setPokemonsActions(value)),}); //recibe el dispatcher de redux y retorna el objeto mapeado pero ahora con los actions creators que habíamos establecido.
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
 
 //conectamos el componente con redux
